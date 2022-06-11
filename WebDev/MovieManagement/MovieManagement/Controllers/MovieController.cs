c56d6a4581f9c6a710a70506f83cb330c9d1b99b
santbohara <santbohara@gmail.com>
2022-06-11 17:44:03 +0545

    Save movie action added

--- WebDev/MovieManagement/MovieManagement/Controllers/MovieController.cs
+++ WebDev/MovieManagement/MovieManagement/Controllers/MovieController.cs
@@ -5,49 +5,74 @@ namespace MovieManagement.Controllers
 {
     public class MovieController : Controller
     {
-        public IActionResult Index()
+        readonly List<Movie> movies = new()
         {
-            Movie movie1 = new()
+            new Movie
             {
                 Name = "3 Idiots",
                 Code = "A1234",
                 Genre = "Comedy",
                 Desc = "3 Idiots is a 2009 Indian Hindi-language coming-of-age comedy-drama film written, edited and directed by Rajkumar Hirani.",
                 Length = 220,
-                imgUrl = "3-idiots.jpeg",
-                ReleaseDate = DateOnly.Parse("08/19/2014")
-            };
-
-            Movie movie2 = new()
+                ImgUrl = "3-idiots.jpeg",
+                ReleaseDate = DateTime.Parse("08/19/2014")
+            },
+            new Movie
             {
                 Name = "KGF 2",
                 Code = "A1235",
                 Genre = "Action",
                 Desc = "The blood-soaked land of Kolar Gold Fields has a new overlord now, Rocky, whose name strikes fear in the heart of his foes. His allies look up to him as their Savior, the government sees him as a threat, and his enemies are clamouring for revenge.",
                 Length = 320,
-                imgUrl = "kgf-2.jpg",
-                ReleaseDate = DateOnly.Parse("01/02/2022")
-            };
-
-            Movie movie3 = new()
+                ImgUrl = "kgf-2.jpg",
+                ReleaseDate = DateTime.Parse("01/02/2022")
+            },
+            new Movie
             {
                 Name = "Pushpa: The Rise",
                 Code = "A1236",
                 Genre = "Action, Drama",
                 Desc = "A labourer named Pushpa makes enemies as he rises in the world of red sandalwood smuggling. However, violence erupts when the police attempt to bring down his illegal business.",
                 Length = 300,
-                imgUrl = "pushpa.jpg",
-                ReleaseDate = DateOnly.Parse("08/19/2021")
-            };
+                ImgUrl = "pushpa.jpg",
+                ReleaseDate = DateTime.Parse("08/19/2021")
+            }
+        };
 
-            List<Movie> movies = new() { movie1, movie2, movie3 };
-
+        public IActionResult Index()
+        {
             return View(movies);
         }
-    
+
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Save(Movie input)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Add");
+            }
+            else
+            {
+                Movie movie = new()
+                {
+                    Name = input.Name,
+                    Code = input.Code,
+                    Genre = input.Genre,
+                    Desc = input.Desc,
+                    Length = input.Length,
+                    ImgUrl = "default.JPG",
+                    ReleaseDate = input.ReleaseDate
+                };
+
+                movies.Add(movie);
+
+                return View("Index", movies);
+            }
         }
     }
 }
--- WebDev/MovieManagement/MovieManagement/Models/Movie.cs
+++ WebDev/MovieManagement/MovieManagement/Models/Movie.cs
@@ -1,13 +1,28 @@
-﻿namespace MovieManagement.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieManagement.Models
 {
     public class Movie
     {
         public string Name { get; set; } = "";
+
+        [DisplayName("Movie Brief Details")]
         public string Desc { get; set; } = "";
         public string Code { get; set; } = "";
+
+        [DisplayName("Choose Genre")]
         public string Genre { get; set; } = "";
-        public DateOnly ReleaseDate { get; set; }
-        public float Length { get; set; }
-        public string imgUrl { get; set; } = "";
+
+        [DataType(DataType.Date)]
+        [DisplayName("Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [DisplayName("Movie Length (in Minute)")]
+        [Range(0, int.MaxValue)]
+        public int Length { get; set; } = int.MinValue;
+
+        [Display(Name = "Upload Banner")]
+        public string ImgUrl { get; set; } = "";
     }
 }
